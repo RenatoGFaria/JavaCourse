a79d6a01e3ecfee8445c6a47c2ba043225ab1531
Renato Faria <renatogomesfaria@gmail.com>
2021-02-24 09:14:52 -0300

    data entry class

---
+++ src/class_entry_data.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class class_entry_data {
+
+	public static void main(String[] args) {
+		
+		
+		 Scanner sc = new Scanner (System.in);
+
+		 String x;
+		 x = sc.next();
+		 System.out.println("You whither: " + x);
+		 
+		 
+		 sc.close();
+	}
+
+}
