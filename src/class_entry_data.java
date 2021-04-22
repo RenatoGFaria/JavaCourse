e95e2a562e5fb512774f5351364304c7b14208b6
Renato Faria <renatogomesfaria@gmail.com>
2021-04-22 18:21:20 -0300

    Class Entry Data

--- src/class_entry_data.java
+++ src/class_entry_data.java
@@ -1,18 +1,30 @@
+
 import java.util.Scanner;
 
-public class class_entry_data {
+public class Class_entry_data {
 
 	public static void main(String[] args) {
 		
-		
-		 Scanner sc = new Scanner (System.in);
 
-		 String x;
-		 x = sc.next();
-		 System.out.println("You whither: " + x);
-		 
-		 
-		 sc.close();
+	Scanner sc = new Scanner(System.in);
+	
+	String x;
+	int y;
+	Double z;
+	
+	x = sc.next();
+	y = sc.nextInt();
+	z = sc.nextDouble();
+	
+	System.out.println("You tape: ");
+	System.out.println(x);
+	System.out.println(y);
+	System.out.println(z);
+	
+	
+	sc.close();
+	
+
 	}
 
 }
